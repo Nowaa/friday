1bd4aa2e1f24fe54ec3016e96541ad509790c73a
Nowa <kloenth@gmail.com>
2020-09-10 19:23:19 -0300

    Add migrations

---
+++ database/migrations/2020_09_10_172336_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('description', 255)->default('-')->nullable(false);
+            $table->string('owner', 60)->default('No owner')->nullable(false);
+            $table->string('status', 50)->default('-')->nullable(false);
+            $table->string('category', 100)->default('-')->nullable(false);
+            $table->timestamp('created-at')->default(now())->nullable(false);
+            $table->dateTime('finished-at')->nullable();
+            $table->string('priority', 20)->default('Low')->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
