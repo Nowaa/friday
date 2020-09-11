d8befd61297a60c4b73511fb80c0927a4c7ce4dd
Nowa <kloenth@gmail.com>
2020-09-11 19:03:13 -0300

    Add load method to the root domain route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'load']);
