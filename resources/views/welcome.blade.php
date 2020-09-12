f4b3282a676291fe7c42953b352830cabb046cef
Nowa <kloenth@gmail.com>
2020-09-12 18:58:08 -0300

    Group tasks by status and change color according to their priority

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,18 +4,25 @@
 
 <body class="antialiased">
 
-    <div class="mt-40">
-        @foreach ($tasks as $task)
-        <div class="px-3 py-3 bg-white shadow-xs rounded mx-4 md:mx-auto my-10 max-w-md md:max-w-2xl">
-            <div class="flex items-center justify-between">
-                <div class="flex items-center ml-8 text-lg font-semibold text-gray-900 -mt-1">{{ $task->description }}</div>
-                <div class="bg-gray-700 py-1 rounded-lg shadow-xs hover:bg-blue-500">
-                    <div class="flex items-center px-4 text-white text-sm">{{$task->category}}</div>
-                </div>
-            </div>
-            <div class="ml-8 text-gray-700 text-xs">Created at {{$task->created_at}}</div>
+    <div class="mt-20">
+        @foreach ($status as $st)
+        <div class="ml-4 md:mx-auto mt-8 max-w md:max-w-2xl">
+            <h1 class="text-lg text-gray-800 border-b-2">
+                {{ $st->name }}
+            </h1>
         </div>
-
+            @foreach ($st->tasks as $task)
+                
+                <div class="px-3 py-2 bg-gray-100 border-l-4 {{ \App\Http\Controllers\TaskController::setPriorityColor($task->priority) ?? '' }} rounded mx-4 md:mx-auto my-5 max-w md:max-w-2xl">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center ml-2 text-gray-700 font-semibold -mt-1 lg:text-md">{{ $task->description }}</div>
+                        <div class="bg-transparent text-xs  border border-gray-700 py-1 rounded-lg text-teal-800 hover:text-teal-100 hover:bg-gray-700">
+                            <div class="flex items-center px-3">{{ $task->category }}</div>
+                        </div>
+                    </div>
+                    <div class="ml-2 text-gray-700 text-xs">Added {{ $task->created_at->diffForHumans() }}</div>
+                </div>
+            @endforeach
         @endforeach
     </div>
 </body>
