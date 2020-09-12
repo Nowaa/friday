138afce2bf526be74864435e76fdd9b1120daaa8
Nowa <kloenth@gmail.com>
2020-09-12 18:37:34 -0300

    Modify load method and add setPriorityColor method

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,13 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Task;
+use App\Models\Status;
 
 class TaskController extends Controller
 {
     public function load()
     {
+        //dd(Status::find(1)->tasks()->get());
+
+        $status = Status::all();
+
         return view('welcome', [
-            'tasks' => Task::all()
+            'status' => $status
         ]);
+    }
+
+    public static function setPriorityColor($priority)
+    {
+        $color = '';
+
+        switch ($priority) {
+            case 'Low':
+                $color = 'border-teal-300';
+                break;
+            case 'Medium':
+                $color = 'border-orange-400';
+                break;
+            case 'High':
+                $color = 'border-red-400';
+                break;         
+        }
+        return $color;
     }
 }
