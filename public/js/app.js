99a16f2709535e157976e3992bf0fb590f2238f5
Nowaa <kloenth@gmail.com>
2020-09-13 18:02:57 -0300

    Rename tw.css to app.css

--- public/js/app.js
+++ public/js/app.js
@@ -19323,10 +19323,10 @@ module.exports = function(module) {
 
 /***/ }),
 
-/***/ "./resources/css/tw.css":
-/*!******************************!*\
-  !*** ./resources/css/tw.css ***!
-  \******************************/
+/***/ "./resources/css/app.css":
+/*!*******************************!*\
+  !*** ./resources/css/app.css ***!
+  \*******************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -19378,14 +19378,14 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!**********************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/css/tw.css ***!
-  \**********************************************************/
+/*!***********************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/css/app.css ***!
+  \***********************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\friday\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! c:\friday\resources\css\tw.css */"./resources/css/tw.css");
+__webpack_require__(/*! /home/Loe/projects/friday/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/Loe/projects/friday/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
